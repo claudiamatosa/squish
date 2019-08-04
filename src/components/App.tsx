b49c2db345ab028814b08c581e15772c1961130b
Claudia Matosa <claudiamatosa@users.noreply.github.com>
2019-08-04 22:31:54 +0100

    Clean up

---
+++ src/components/App.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+export interface AppProps { compiler: string; framework: string; }
+
+const App = (props: AppProps) => <h1>Hello from {props.compiler} and {props.framework}!</h1>;
+
+export default App;
