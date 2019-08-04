51fa8864e5be3474464cc78fd6fdc9045813f151
Claudia Matosa <claudiamatosa@users.noreply.github.com>
2019-08-04 23:58:47 +0100

    Add responsive grid

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -8,11 +8,27 @@ export interface GalleryProps {
 
 const Gallery = styled.ul`
   line-height: 0;
-  column-count: 4;
+  column-count: 1;
   column-gap: 10px;
   list-style: none;
   padding: 0;
   margin: 0;
+
+  @media (min-width: 400px) {
+    column-count: 2;
+  }
+
+  @media (min-width: 768px) {
+    column-count: 3;
+  }
+
+  @media (min-width: 1024px) {
+    column-count: 4;
+  }
+
+  @media (min-width: 1308px) {
+    column-count: 5;
+  }
 `;
 
 const Item = styled.li`
